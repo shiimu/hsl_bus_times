3725a7f7181d032afdc04e137036c73ae8e2aeec
shiimu <siim.laineste@outlook.com>
2021-12-05 21:55:35 +0200

    :construction_worker: added time object + figured out how to implement frontend

--- hsl_bus_times/main.py
+++ hsl_bus_times/main.py
@@ -1,20 +1,17 @@
-"""
-main.py
-Author: @shiimu
-Date: 01/12/2021
-"""
-
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, jsonify
 import time
 import requests
+from requests.models import Response
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    from query_stop_times import queryApi
+    queryApi('HSL:1472128')
     return render_template('index.html')
 
-# Runs at localhost:5000
+
 if __name__ == "__main__":
-    app.run( debug = True )
+    app.run(debug=True)
--- hsl_bus_times/query_stop_times.py
+++ hsl_bus_times/query_stop_times.py
@@ -1,20 +1,25 @@
 import requests
 import json
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client['weather_info']
+db = client['hsl_bus_times']
 collection = db['bus_collection']
+
 
 def queryApi(stop_id):
     url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
-    payload = {"query":"{\n  stop(id: \"" + stop_id + "\") {  name   stoptimesWithoutPatterns{realtimeDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
-    headers= {"Content-Type" : "application/json"}
-    response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
+    payload = {"query": "{\n  stop(id: \"" + stop_id +
+               "\") {  name   stoptimesWithoutPatterns{realtimeDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
+    headers = {"Content-Type": "application/json"}
+    response = requests.request(
+        "POST", url, headers=headers, data=json.dumps(payload))
 
     global dumped_data
     dumped_data = response.json()
     # ADD TO DB
+    busToDB()
+
 
 def busToDB():
-
     collection.insert(dumped_data)
